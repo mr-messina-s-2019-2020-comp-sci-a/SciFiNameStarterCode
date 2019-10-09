c4246e7f088a10a06988ca3ca922069d5b255c53
gharper11 <55795447+gharper11@users.noreply.github.com>
2019-10-09 08:04:21 -0500

    Add files via upload

---
+++ Mathh.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Math here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Mathh
+{
+    public double r = Math.random();
+    
+    public static void main(String[] args)
+    {
+        System.out.println((int) (Math.random() * 10));
+    }
+}
